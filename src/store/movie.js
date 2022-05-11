2e4acf3fdfadf1a96b8d94fc6f443b5de825fcd0
StarkLSH <lsh@zipdoc.kr>
2022-05-11 22:35:45 +0900

    movieitem data binding

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,35 +1,53 @@
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   namespaced: true,
   state: () => ({
-      movies: [],
-      message: '',
-      loading: false
+    movies: [],
+    message: "",
+    loading: false,
   }),
   getters: {},
   mutations: {
     updateState(state, payload) {
-      Object.keys(payload).forEach(key => {
-        state[key] = payload[key]
-      })
+      Object.keys(payload).forEach((key) => {
+        state[key] = payload[key];
+      });
     },
     resetMovies(state) {
-      state.movies = []
-    }
+      state.movies = [];
+    },
   },
   actions: {
-    async searchMovies({commit}, payload) {
+    async searchMovies({ state, commit }, payload) {
       // 구조 분해
-      const {title, type, number, year} = payload
-      const OMDB_API_KEY = '7035c60c'
-      const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
-      const {Search, totalReults} = res.data
-      
-      commit('updateState', {
-        movies: Search,
-      })
-    }
-  },
+      const { title, type, number, year } = payload;
+      const OMDB_API_KEY = "7035c60c";
+      const res = await axios.get(
+        `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`
+      );
+      const { Search, totalReults } = res.data;
 
-}
+      commit("updateState", {
+        movies: Search,
+      });
+      console.log(totalReults); // 268 => 27
+      console.log(typeof totalReults); // string
+
+      const total = parseInt(totalReults, 10);
+      const pageLength = Math.ceil(total / 10);
+
+      if (pageLength > 1) {
+        for (let page = 2; page <= pageLength; page += 1) {
+          const res = await axios.get(
+            `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+          );
+          const { Search } = res.data;
+          commit("updateState", {
+            movies: [...state.movies, ...Search],
+          });
+        }
+      }
+    },
+  },
+};
