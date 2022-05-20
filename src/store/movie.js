57277da48e650d3a942c8cf84ca4575fdf799e2c
StarkLSH <lsh@zipdoc.kr>
2022-05-20 15:43:54 +0900

    lodash 추가, 영화 ID 중복 제거

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import _uniqBy from 'lodash/uniqBy'
 
 export default {
   namespaced: true,
@@ -26,16 +27,14 @@ export default {
       const res = await axios.get(
         `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`
       );
-
       const { Search, totalResults } = res.data;
 
       commit("updateState", {
-        movies: Search,
+        movies: _uniqBy(Search, 'imdbID'),
       });
 
       console.log(totalResults)
       console.log(typeof totalResults)
-
 
       const total = parseInt(totalResults, 10);
       const pageLength = Math.ceil(total / 10);
@@ -49,7 +48,10 @@ export default {
           );
           const { Search } = res.data;
           commit("updateState", {
-            movies: [...state.movies, ...Search],
+            movies: [
+              ...state.movies, 
+              ..._uniqBy(Search, 'imdbID')
+            ],
           });
         }
       }
