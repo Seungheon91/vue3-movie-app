4948307fcc42beab2e695a8c3494fbf84165c849
lsh <seungheon1029@gmail.com>
2022-08-27 17:41:34 +0900

    api 재사용성 개선, 포스터 이미지 기능 추가

--- src/store/movie.js
+++ src/store/movie.js
@@ -21,41 +21,65 @@ export default {
   },
   actions: {
     async searchMovies({ state, commit }, payload) {
-      // 구조 분해
-      const { title, type, number, year } = payload;
-      const OMDB_API_KEY = "7035c60c";
-      const res = await axios.get(
-        `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`
-      );
-      const { Search, totalResults } = res.data;
-
-      commit("updateState", {
-        movies: _uniqBy(Search, 'imdbID'),
-      });
-
-      console.log(totalResults)
-      console.log(typeof totalResults)
-
-      const total = parseInt(totalResults, 10);
-      const pageLength = Math.ceil(total / 10);
-
-      if (pageLength > 1) {
-        for (let page = 2; page <= pageLength; page += 1) {
-          console.log(page)
-          if (page > (number / 10)) break
-          const res = await axios.get(
-            `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-          );
-          const { Search } = res.data;
-          commit("updateState", {
-            movies: [
-              ...state.movies, 
-              ..._uniqBy(Search, 'imdbID')
-            ],
-          });
+      try {
+        const res = await _fetchMovie({
+          ...payload,
+          page: 1
+        })
+        const { Search, totalResults } = res.data;
+  
+        commit("updateState", {
+          movies: _uniqBy(Search, 'imdbID'),
+        });
+  
+        console.log(totalResults)
+        console.log(typeof totalResults)
+  
+        const total = parseInt(totalResults, 10);
+        const pageLength = Math.ceil(total / 10);
+  
+        if (pageLength > 1) {
+          for (let page = 2; page <= pageLength; page += 1) {
+            console.log(page)
+            if (page > (payload.number / 10)) break
+            const res = await _fetchMovie({...payload, page})
+            const { Search } = res.data;
+            commit("updateState", {
+              movies: [
+                ...state.movies, 
+                ..._uniqBy(Search, 'imdbID')
+              ],
+            });
+          }
         }
+      } catch (message) {
+        commit('updateState', {
+          movies: [],
+          message
+        })
       }
-      console.log(state.movies)
-    },
+      
   },
-};
+}
+}
+
+function _fetchMovie(payload) {
+  const {title, type, page, year} = payload;
+  const OMDB_API_KEY = "7035c60c";
+  const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+  //const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}`
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+    .then(res=> {
+      console.log(res)
+      if(res.data.Error) {
+        reject(res.data.Error)
+      }
+      resolve(res)
+    })
+    .catch(err => {
+      reject(err)
+    })
+  })
+}
+
