c21c731aeeda6e8d3a069231f1cc0f3747c674ba
lsh <seungheon1029@gmail.com>
2022-09-25 12:25:33 +0900

    메인페이지 반응형 작업

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -7,7 +7,7 @@
         {{ movie.Year }}
       </div>
       <div class="title">
-        {{ movie.Title }}
+        {{ movie.Title }}   
       </div>
     </div>
   </div>
@@ -37,7 +37,16 @@ export default {
     background-size: cover;
     overflow: hidden;
     position: relative;
-    .info{
+    &:hover::after {
+      content:"";
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      border: 6px solid $primary;
+    }
+    .info {
       background-color: rgba($black, .3);
       width: 100%;
       padding: 14px;
@@ -46,6 +55,16 @@ export default {
       position: absolute;
       left:0;
       bottom: 0;
+      backdrop-filter: blur(10px);
+    }
+    .year {
+      color: $primary
+    }
+    .title {
+      color: $white;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
     }
   }
 </style>
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -5,37 +5,40 @@
         {{ message }}
       </div>
       <div class="movies">
-        <MovieItem
-          v-for="movie in movies"
-          :key="movie.imdbID"
-          :movie="movie" />
+        <MovieItem v-for="movie in movies" :key="movie.imdbID" :movie="movie" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import MovieItem from '~/components/MovieItem'
+import MovieItem from "~/components/MovieItem";
 
 export default {
   components: {
-    MovieItem
+    MovieItem,
   },
   computed: {
     movies() {
-      return this.$store.state.movie.movies
+      return this.$store.state.movie.movies;
     },
     message() {
-      return this.$store.state.movie.message
-    }
-  }
-
-}
+      return this.$store.state.movie.message;
+    },
+  },
+};
 </script>
 <style lang="scss" scoped>
-.container{
-  .movies{
-    display:flex;
+@import "~/scss/main";
+.container {
+  margin-top: 30px;
+  .inner {
+    background-color: $gray-200;
+    padding: 10px 0;
+    border-radius: 4px;
+  }
+  .movies {
+    display: flex;
     justify-content: center;
     flex-wrap: wrap;
   }
