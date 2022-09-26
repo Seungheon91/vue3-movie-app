7560213a12cca1701547a2b076e8eb8e0b306647
StarkLSH <lsh@zipdoc.kr>
2022-09-26 22:13:24 +0900

    Footer 추가

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer>
+    <Logo />
+    <a
+      href="https://github.com/Seungheon91"
+      target="_blank">(c){{ new Date().getFullYear() }} seungheon</a>
+  </footer>
+</template>
+<script>
+import Logo from "~/components/Logo";
+export default {
+  components: {
+    Logo,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+footer {
+  padding: 70px;
+  text-align: center;
+  opacity: 0.3;
+  .logo {
+    display: block;
+    margin-bottom: 4px;
+  }
+}
+</style>
