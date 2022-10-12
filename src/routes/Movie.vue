1498a36eed648adf942dec76cafef50587e0186b
StarkLSH <lsh@zipdoc.kr>
2022-10-12 18:04:45 +0900

    영화 포스터 해상도 높이는 함수 추가

--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -21,7 +21,7 @@
       v-else
       class="movie-details">
       <div
-        :style="{backgroundImage: `url(${theMovie.Poster})`}"
+        :style="{backgroundImage: `url(${requestDiffSizeImage(theMovie.Poster)})`}"
         class="poster">
       </div>
       <div class="specs">
@@ -92,6 +92,11 @@ export default {
     this.$store.dispatch('movie/searchMovieWithId', {
       id: this.$route.params.id
     })
+  },
+  methods: {
+    requestDiffSizeImage(url, size = 700){
+      return url.replace('SX300', `SX${size}`)
+    }
   }
 }
 </script>
