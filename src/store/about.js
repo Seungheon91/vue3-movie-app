30f017f15a2a94177ff69e46a36b3c295b9c4253
StarkLSH <lsh@zipdoc.kr>
2022-10-13 17:24:21 +0900

    about 페이지 내용 추가

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'SEUNGHEON',
+    email: 'seungheon1029@gmail.com',
+    blog: 'https://yuien.tistory.com',
+    phone: '+82-10-1234-5678',
+    image: require('~/assets/about.jpg')
+  })
+}
