518c646a47f6d308aad1f6dc15e5e745eebddd5a
StarkLSH <lsh@zipdoc.kr>
2022-10-13 18:05:46 +0900

    메인페이지 반응형 작업 추가

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,6 +15,13 @@
         </RouterLink>
       </div>
     </div>
+    <div
+      class="user"
+      @click="toAbout">
+      <img
+        :src="image"
+        :alt="name" />
+    </div>
   </header>
 </template>
 
@@ -42,10 +49,21 @@
         ],
       };
     },
+    computed: {
+      image() {
+        return this.$store.state.about.image
+      },
+      name() {
+        return this.$store.state.about.name
+      }
+    },
     methods: {
       isMatch(path) {
         if (!path) return false
         return path.test(this.$route.fullPath)
+      },
+      toAbout() {
+        this.$router.push('/about')
       }
     }
   };
@@ -59,9 +77,37 @@
     padding: 0 40px;
     display: flex;
     align-items: center;
+    position:relative;
 
     .logo {
       margin-right: 40px;
+    }
+
+    .user {
+      width: 40px;
+      height: 40px;
+      padding: 6px;
+      border-radius: 50%;
+      box-sizing: border-box;
+      background-color: $gray-200;
+      cursor: pointer;
+      position: absolute;
+      top:0;
+      bottom: 0;
+      right: 40px;
+      margin: auto;
+      transition: .4s;
+      &:hover {
+        background-color: darken($gray-200, 10%);
+      }
+      img {
+        width: 100%;
+      }
+    }
+    @include media-breakpoint-down(sm) {
+      .nav {
+        display: none;
+      }
     }
   }
 </style>
